c8037c9cada52195f0f1c3e252e5ba34521fae9a
naaf <serranashraf@gmail.com>
2017-02-27 07:12:58 +0100

    detect

--- src/mypackage/Parametrage.java
+++ src/mypackage/Parametrage.java
@@ -3,6 +3,6 @@ package mypackage;
 public class Parametrage {
 	public final static String NOM_EQUIPE = "LES MAK";
 	public final static String MAIN_NOM_CLASS = "mypackage.Exo1";
-	public final static String SECONDAIRE_NOM_CLASS = "mypackage.Exo1";
+	public final static String SECONDAIRE_NOM_CLASS = "mypackage.Stage1_mieux";
 
 }
